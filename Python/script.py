74a7c03e0d44915fe3beb74bf6e025bfc1eed326
Katbie Matheo <matheo.okatbie@icloud.com>
2024-01-09 17:07:48 +0100

    Update docker and python

--- Python/script.py
+++ Python/script.py
@@ -1,13 +1,29 @@
-
-
 import paho.mqtt.client as mqtt
 import json
 import base64
+import pymysql
+
+# connection to db
+connection = pymysql.connect(
+    host="localhost", 
+    port=9906, 
+    user="USER", 
+    passwd="PASS", 
+    database="php-app")
+cursor = connection.cursor()
+
+query = "SELECT * FROM test;"
+
+cursor.execute(query)
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+connection.close()
 
 # config
 mqttServer = "chirpstack.iut-blagnac.fr"
-appID = "6"
-deviceID = "**DEVEUI de votre device**"
 
 blue = "Ymx1ZQ=="
 orange = "b3Jhbmdl"
@@ -16,7 +32,7 @@ print("Starting...")
 
 # callback appele lors de la reception d'un message
 def get_data(mqttc, obj, msg):
-    print(msg)
+    print(msg,obj,mqttc)
     pass
 
 # creation du client
@@ -26,7 +42,9 @@ mqttc.connect(mqttServer, port=1883, keepalive=60)
 mqttc.on_message = get_data
 
 # soucription au device
-mqttc.subscribe("application/"+appID+"/device/"+deviceID+"/event/up", 0)
+#mqttc.subscribe("application/"+appID+"/device/+/event/up", 0)
+mqttc.subscribe("AM107/by-deviceName/+/data",0)
+print("Connected !")
 
 mqttc.loop_forever()
 
