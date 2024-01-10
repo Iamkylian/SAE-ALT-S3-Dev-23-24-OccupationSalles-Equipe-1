1d60af7f53f7211eabc2b28ffe5b57df0249934e
Katbie <matheo.okatbie@icloud.com>
2024-01-10 10:46:37 +0100

    Update python file and sql file

--- BDD/db.sql
+++ BDD/db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : db
--- Généré le : mer. 10 jan. 2024 à 08:54
+-- Généré le : mer. 10 jan. 2024 à 09:45
 -- Version du serveur : 8.2.0
 -- Version de PHP : 8.2.14
 
@@ -32,10 +32,34 @@ USE `capteursDB`;
 CREATE TABLE `Device` (
   `id` int NOT NULL,
   `deviceName` varchar(50) NOT NULL,
-  `room` varchar(10) NOT NULL,
+  `room` varchar(25) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
   `floor` int NOT NULL,
   `building` char(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Déchargement des données de la table `Device`
+--
+
+INSERT INTO `Device` (`id`, `deviceName`, `room`, `floor`, `building`) VALUES
+(12, 'AM107-43', 'Salle-conseil', 1, 'A'),
+(13, 'AM107-16', 'B109', 1, 'B'),
+(14, 'AM107-31', 'E101', 1, 'E'),
+(15, 'AM107-3', 'B111', 1, 'B'),
+(16, 'AM107-42', 'E210', 2, 'E'),
+(17, 'AM107-2', 'B110', 1, 'B'),
+(18, 'AM107-37', 'E100', 1, 'E'),
+(19, 'AM107-30', 'E007', 0, 'E'),
+(20, 'AM107-6', 'B203', 2, 'B'),
+(21, 'AM107-9', 'B003', 0, 'B'),
+(22, 'AM107-15', 'B108', 1, 'B'),
+(23, 'AM107-29', 'E006', 0, 'E'),
+(24, 'AM107-23', 'C004', 0, 'C'),
+(25, 'AM107-28', 'E001', 0, 'E'),
+(26, 'AM107-5', 'B202', 2, 'B'),
+(27, 'AM107-27', 'E004', 0, 'E'),
+(28, 'AM107-24', 'C006', 0, 'C'),
+(29, 'AM107-26', 'E003', 0, 'E');
 
 -- --------------------------------------------------------
 
@@ -77,7 +101,7 @@ ALTER TABLE `Donnes`
 -- AUTO_INCREMENT pour la table `Device`
 --
 ALTER TABLE `Device`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- Contraintes pour les tables déchargées
--- Python/script.py
+++ Python/script.py
@@ -9,18 +9,16 @@ connection = pymysql.connect(
     port=9906, 
     user="root", 
     passwd="ROOT_PASSWORD", 
-    database="donnesDB")
+    database="capteursDB")
 cursor = connection.cursor()
 
-query = "SELECT * FROM Donnee;"
+query = "SELECT * FROM Donnes;"
 
 cursor.execute(query)
 rows = cursor.fetchall()
 
 for row in rows:
     print(row)
-
-connection.close()
 
 # config
 mqttServer = "chirpstack.iut-blagnac.fr"
@@ -32,7 +30,41 @@ print("Starting...")
 
 # callback appele lors de la reception d'un message
 def get_data(mqttc, obj, msg):
-    print(msg.topic + str(msg.payload))
+    print(msg.topic + "\n\n")
+    data = json.loads(msg.payload)
+    print(data)
+    
+    insertDevice(data[1]['deviceName'],data[1]['room'],data[1]['floor'],data[1]['Building'])
+    insertData(data[1]['deviceName'],data[0]['temperature'],data[0]['humidity'],data[0]['activity'],data[0]['co2'],data[0]['tvoc'],data[0]['illumination'])
+
+    pass
+
+def insertDevice(deviceName,room,floor,building):
+    query = "SELECT COUNT(*) FROM Device WHERE deviceName = '" + deviceName + "';"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        count = row[0]
+        print(count)
+
+    if count == 0:
+        query = "INSERT INTO Device (id,deviceName,room,floor,building) VALUES (NULL,'" + str(deviceName) + "','" + str(room) + "'," + str(floor) + ",'" + str(building) + "');"
+        print(query)
+        cursor.execute(query)
+        connection.commit()
+    else:
+        print("Already inserted")
+        pass
+
+def insertData(deviceName, temperature,humidity,activity,co2,tvoc,illumination):
+    query = "SELECT id FROM Device WHERE deviceName = '" +  deviceName + "';"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        deviceId = row[0]
+        print(deviceId)
 
 # creation du client
 mqttc = mqtt.Client()
@@ -47,3 +79,4 @@ print("Connected !")
 
 mqttc.loop_forever()
 
+connection.close()
