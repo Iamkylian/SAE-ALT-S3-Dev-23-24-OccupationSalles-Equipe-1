7614e3b257b4ec33ae9bbd7c78003301d8f6656b
Lylian Derramond <dert3k@gmail.com>
2024-01-12 10:35:08 +0100

    ajout menu dynamiques

--- Docker/php/Parts/navbar.php
+++ Docker/php/Parts/navbar.php
@@ -2,12 +2,28 @@
 
 $rootDir = realpath($_SERVER["DOCUMENT_ROOT"]);
 
+// Obtenez le chemin de la page actuelle
+$currentPath = $_SERVER['REQUEST_URI'];
+
+// Fonction pour vérifier si la page actuelle correspond à une page spécifique
+function isCurrentPage($currentPage, $currentPath) {
+    return strpos($currentPath, $currentPage) !== false;
+}
+
 ?>
 
 <div class="navbar">
-    <button class="nav-btn"><a href="Carte">Carte</a></button>
-    <button class="nav-btn"><a href="Maintenance/">Maintenance</a></button>
-    <button class="nav-btn"><a href="#">Statistique</a></button>
+    <?php if (!isCurrentPage('Carte', $currentPath)) { ?>
+        <button class="nav-btn"><a href="Carte">Carte</a></button>
+    <?php } ?>
+    
+    <?php if (!isCurrentPage('Maintenance', $currentPath)) { ?>
+        <button class="nav-btn"><a href="Maintenance/">Maintenance</a></button>
+    <?php } ?>
+    
+    <?php if (!isCurrentPage('Statistique', $currentPath)) { ?>
+        <button class="nav-btn"><a href="#">Statistique</a></button>
+    <?php } ?>
 </div>
 
 
