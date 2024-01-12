cda028d5b04c068215e9af31f3970031331ddd65
test <matheo.okatbie@icloud.com>
2024-01-12 09:17:46 +0100

    Update repository

---
+++ Docker/php/Parts/navbar.php
@@ -0,0 +1,7 @@
+<div class="navbar">
+    <button class="nav-btn"><a href="Carte/">Carte</a></button>
+    <button class="nav-btn"><a href="Maintenance/">Maintenance</a></button>
+    <button class="nav-btn"><a href="#">Statistique</a></button>
+</div>
+
+
