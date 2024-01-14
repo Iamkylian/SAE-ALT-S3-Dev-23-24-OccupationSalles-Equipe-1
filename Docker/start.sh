466400a5423cffc2f10d7580ebcf75d8a40a1e1e
BlackWindows631 <matheo.okatbie@icloud.com>
2024-01-14 20:20:46 +0100

    Update test file and add test to dockerfile

--- Docker/start.sh
+++ Docker/start.sh
@@ -3,5 +3,7 @@
 python3 -m pip install paho-mqtt
 pip install cryptography
 pip install pymysql 
+python -u Test-Device.py &
+python -u Test-Battery.py &
 python -u device.py &
 python -u battery.py
