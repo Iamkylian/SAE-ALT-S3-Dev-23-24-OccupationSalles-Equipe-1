7c7b29a7dbcd6f8856be8563db1cb55d71c820f9
BlackWindows631 <matheo.okatbie@icloud.com>
2024-01-24 19:51:44 +0100

    Update css

--- Docker/php/Parts/navbar.php
+++ Docker/php/Parts/navbar.php
@@ -39,7 +39,7 @@ function isCurrentPage($currentPage, $currentPath) {
             <li><a href="../Carte/">Carte</a></li>
             <li><a href="../Maintenance/">Maintenance</a></li>
             <li><a href="../Statistiques/">Statistiques</a></li>
-            <li><button id="closeTabBtn" class="btn btn-danger">Quitter application</button></li>
+            <!--<li><button id="closeTabBtn" class="btn btn-danger">Quitter application</button></li>-->
         </ul>
     </nav>
 </header>
