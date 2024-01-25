239a3daac896f98bf7c3f30e294289b2fd38de17
YannRouquie <yann.rouquie@etu.univ-tlse2.fr>
2024-01-25 21:57:09 +0100

    Update docker-compose

--- Docker/start.sh
+++ Docker/start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-python3 -m pip install paho-mqtt
-pip install cryptography
-pip install pymysql 
 python3 -m unittest Test-Device.py &
 python3 -m unittest Test-Battery.py & 
 python -u device.py &
